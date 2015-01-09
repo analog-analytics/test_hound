c467eeed8a847de182c307551b2b1ab016afe526
Ryan Stawarz <ryan@stawarz.com>
2015-01-09 11:02:01 -0600

    modify collection methods

--- verify_hound_checks.rb
+++ verify_hound_checks.rb
@@ -10,4 +10,44 @@ class VerifyHoundChecks
 
     puts str
   end
+
+  def check_collection_methods
+    array = [:a, :b, :c]
+
+    #bad
+    array.collect {|i| i }
+
+    #good
+    array.map {|i| i }
+
+    #bad
+    array.collect! {|i| i }
+
+    #good
+    array.map! {|i| i }
+
+    #bad
+    array.reduce {|i| i }
+
+    #good
+    array.inject {|i| i }
+
+    #bad
+    array.detect {|i| i }
+
+    #good
+    array.find {|i| i }
+
+    #bad
+    array.find_all {|i| i }
+
+    #good
+    array.select {|i| i }
+
+    # bad
+    array.reverse.each {|item| item }
+
+    # good
+    array.reverse_each {|item| item }
+  end
 end
